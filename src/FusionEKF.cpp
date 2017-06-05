c1b5ee207f35855cb3fe1c1109a4b0819bc4c263
Unknown <axel.durbec@gmail.com>
2017-06-05 11:39:26 +0200

    BUG: Lidar H init missing

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -20,6 +20,8 @@ FusionEKF::FusionEKF() {
   R_laser_ = MatrixXd(2, 2);
   R_radar_ = MatrixXd(3, 3);
   H_laser_ = MatrixXd(2, 4);
+  H_laser_ << 1,0,0,0,
+             0,1,0,0;
   Hj_ = MatrixXd(3, 4);
   auto Q = MatrixXd(4, 4);
   Q << 0,0,0,0,
@@ -68,7 +70,9 @@ void FusionEKF::SetLaserMatrices()
 
 void FusionEKF::UpdateFandQ(const MeasurementPackage &measurement_pack)
 {
-    auto delta_t = measurement_pack.timestamp_ - previous_timestamp_;
+    constexpr double MICROSECS_TO_SECS = 1/1000000.0;
+    auto delta_t = (measurement_pack.timestamp_ - previous_timestamp_)*MICROSECS_TO_SECS;
+    //cout << "Delta t: " << delta_t << endl;
     ekf_.F_ << 1,0,delta_t,0,
                0,1,0,delta_t,
                0,0,1,0,
@@ -106,10 +110,10 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.P_ = MatrixXd(4, 4);
     // Initial cov set to 2m for position and 1m/s for speed
     cout << "Init P cov matrix:" << endl;
-    ekf_.P_ << 2,0,0,0,
-               0,2,0,0,
-               0,0,1,0,
-               0,0,0,1;
+    ekf_.P_ << 3,0,0,0,
+               0,3,0,0,
+               0,0,3,0,
+               0,0,0,3;
     cout << ekf_.P_ << endl;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
@@ -124,7 +128,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       */
         ekf_.x_ <<  measurement_pack.raw_measurements_(0),
                 measurement_pack.raw_measurements_(1),
-                0,0;
+               0.5,0.5;
     }
     previous_timestamp_ = measurement_pack.timestamp_;
 
@@ -152,12 +156,12 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-      cout << "Radar meas. " << endl;
+      //cout << "Radar meas. " << endl;
       //SetRadarMatrices();
       //ekf_.UpdateEKF(z);
   } else {
     // Laser updates
-      cout << "Laster meas. " << endl;
+      //cout << "Laster meas. " << endl;
       SetLaserMatrices();
       ekf_.Update(measurement_pack.raw_measurements_);
   }
